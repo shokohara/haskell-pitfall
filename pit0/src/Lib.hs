1a6e66944be9c7e9a64a751459e70062491907f1
shokohara <shokohara@users.noreply.github.com>
2016-11-21 17:51:29 +0900

    Fix pit0

--- pit0/src/Lib.hs
+++ pit0/src/Lib.hs
@@ -7,7 +7,7 @@ import B
 
 -- error: Qualified name in binding position: A.A
 -- error: Qualified name in binding position: B.B
-data AB = A.A | B.B
+data AB = AA A.A | BB B.B
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
